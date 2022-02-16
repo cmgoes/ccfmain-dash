b92d4de95a2c7260fe6bca9092a8f97575143519
unknown <brewerkberly@gmail.com>
2022-02-16 14:13:01 +0900

    circulating supply, total supply, burnt fixed

--- src/components/layout-components/NavPanel.js
+++ src/components/layout-components/NavPanel.js
@@ -4,10 +4,13 @@ import { Drawer, Menu, Button, Dropdown } from 'antd';
 import ThemeConfigurator from './ThemeConfigurator';
 import { connect } from "react-redux";
 import axios from 'axios';
-import ccfTokenAbi from './abi/ccf-abi';
+import tokenAbi from './abi/ccf-abi';
 
 const Web3 = require('web3');
 const tokenAddress = "0x7f9528b913A99989B88104b633D531241591A358";
+const lptokenAddress = "0x83a0962aE816604a6b162a5E054912982C8e4C1C";
+const wbnbTokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const busdTokenAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
 const NavPanel = () => {
   const [visible, setVisible] = useState(false)
@@ -30,22 +33,20 @@ const NavPanel = () => {
 		account = accounts[0];
 		return account;
   }
-  const getBalanceValue = () => {
-		const ids = "cross-chain-farming";
-		const vs_currencies = "usd";
-
-		axios.get("https://api.coingecko.com/api/v3/simple/price", { params: { "ids": ids, "vs_currencies": vs_currencies } })
-		  .then(res => {
-			setCurrentPrice(res.data[ids].usd.toString());
-		  })
-		  .catch(err => {
-	
-			console.log(err);
-		  });
-	  }
-	  const getBalance = userAddress => {
+  const getBalanceValue = async () => {
 		const provider = new Web3(window.web3.currentProvider);
-		var ccfContract = new provider.eth.Contract(ccfTokenAbi, tokenAddress);
+		const ccfContract = new provider.eth.Contract(tokenAbi, tokenAddress);
+		const wbnbContract = new provider.eth.Contract(tokenAbi, wbnbTokenAddress);
+		const busdContract = new provider.eth.Contract(tokenAbi, busdTokenAddress);
+		const ccfbalanceOfLP = (await ccfContract.methods.balanceOf(lptokenAddress).call()) / 10 ** 9;
+		const wbnbbalanceOfLP = (await wbnbContract.methods.balanceOf(lptokenAddress).call()) / 10 ** 18;
+		const priceOfBNB = (await busdContract.methods.balanceOf("0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16").call()) / (await wbnbContract.methods.balanceOf("0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16").call())
+		const ccfPrice = wbnbbalanceOfLP / ccfbalanceOfLP * priceOfBNB
+		setCurrentPrice(ccfPrice)
+	}
+	const getBalance = userAddress => {
+		const provider = new Web3(window.web3.currentProvider);
+		var ccfContract = new provider.eth.Contract(tokenAbi, tokenAddress);
 	
 		ccfContract.methods.balanceOf(userAddress).call().then(res => {
 	
@@ -54,17 +55,15 @@ const NavPanel = () => {
 	
 		  console.log(err);
 		});
-  };
-  const initWallet = async () => {
+  	};
+  	const initWallet = async () => {
 		await getAccount().then(res => {
-	
-		  const _currentAccount = res;
-	
-		  setCurrentAccountAddress(_currentAccount);
-		  getBalance(_currentAccount);
-		  getBalanceValue();
+			const _currentAccount = res;
+			setCurrentAccountAddress(_currentAccount);
+			getBalance(_currentAccount);
+			getBalanceValue();
 		}).catch(err => {
-		  console.log(err);
+		  	console.log(err);
 		});
   }
   const clickConnectWallet = () => {
--- src/views/app-views/dashboards/analytic/index.js
+++ src/views/app-views/dashboards/analytic/index.js
@@ -1,261 +1,185 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Card, Avatar, Dropdown, Table, Menu } from 'antd';
+import { Row, Col } from 'antd';
 import StatisticWidget from 'components/shared-components/StatisticWidget';
-import ChartWidget from 'components/shared-components/ChartWidget';
-import {
-  VisitorChartData,
-  AnnualStatisticData,
-} from './DefaultDashboardData';
-import ApexChart from "react-apexcharts";
-import { apexLineChartDefaultOption, COLOR_2 } from 'constants/ChartConstant';
-import {
-  UserAddOutlined,
-  FileExcelOutlined,
-  PrinterOutlined,
-  PlusOutlined,
-  EllipsisOutlined,
-  StopOutlined,
-  ReloadOutlined
-} from '@ant-design/icons';
-import utils from 'utils';
 import { withRouter } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import axios from 'axios';
-import ccfTokenAbi from './abi/ccf-abi';
+import tokenAbi from './abi/ccf-abi';
 import ccfWbnbLpTokenAbi from './abi/ccf-bnb-abi'
 const Web3 = require('web3');
 const tokenAddress = "0x7f9528b913A99989B88104b633D531241591A358";
 const lptokenAddress = "0x83a0962aE816604a6b162a5E054912982C8e4C1C";
+const wbnbTokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const busdTokenAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 const deadAddress = "0x000000000000000000000000000000000000dead";
 
-const latestTransactionOption = (
-  <Menu>
-    <Menu.Item key="0">
-      <span>
-        <div className="d-flex align-items-center">
-          <ReloadOutlined />
-          <span className="ml-2">Refresh</span>
-        </div>
-      </span>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <span>
-        <div className="d-flex align-items-center">
-          <PrinterOutlined />
-          <span className="ml-2">Print</span>
-        </div>
-      </span>
-    </Menu.Item>
-    <Menu.Item key="12">
-      <span>
-        <div className="d-flex align-items-center">
-          <FileExcelOutlined />
-          <span className="ml-2">Export</span>
-        </div>
-      </span>
-    </Menu.Item>
-  </Menu>
-);
-
-const cardDropdown = (menu) => (
-  <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
-    <a href="/#" className="text-gray font-size-lg" onClick={e => e.preventDefault()}>
-      <EllipsisOutlined />
-    </a>
-  </Dropdown>
-)
-
-const tableColumns = [
-  {
-    title: 'METRIC',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text, record) => (
-      <div className="d-flex align-items-center">
-        <Avatar size={30} className="font-size-sm" style={{ backgroundColor: record.avatarColor }}>
-          {utils.getNameInitial(text)}
-        </Avatar>
-        <span className="ml-2">{text}</span>
-      </div>
-    ),
-  },
-  {
-    title: 'VALUE',
-    dataIndex: 'date',
-    key: 'date',
-  },
-
-];
-
 export const DefaultDashboard = () => {
-  const [visitorChartData] = useState(VisitorChartData);
-  const [annualStatisticData] = useState(AnnualStatisticData);
-  const [manualburntToken, setburntToken] = useState();
-  const [lpTokenAmount, setLpTokenAmount] = useState();
-  const [series, setSeries] = useState(VisitorChartData.series);
-  const [lpTokenTotalSupply, setLpTokenTotalSupply] = useState();
-  const [recentTransactionData , setrecentTransactionData] = useState([]);
-  const { direction } = useSelector(state => state.theme)
+  const [recentTransactionData , setrecentTransactionData] = useState([
+    {
+      title: 'Market Cap',
+      value: '$ 0'
+    },
+    {
+      title: 'CCF/BNB Liquidity',
+      value: '$ 0',
+    },
+    {
+      title: '24H Volume',
+      value: '$ 0',
+    },
+    {
+      title: 'Total supply',
+      value: 0,
+    },
+    {
+      title: 'Circulating supply',
+      value: 0,
+    },
+    {
+      title: 'Burnt token',
+      value: 0,
+    }, {
+      title: 'Burning rate',
+      value: '0 %',
+    },
+    {
+      title: 'Pancakeswap Price',
+      value: '$ 0'
+    },
+    {
+      title: 'Holders',
+      value: 0
+    },
+    {
+      title: 'Market Cap(Fully Dilluted)',
+      value: '$ 0'
+    },
+    {
+      title: 'All-Time High',
+      value: '$ 0'
+    }
+  ]);
 
   const params = {
     "ids": "cross-chain-farming",
     "vs_currency": "usd"
   };
-  const [token, setToken] = useState({ max_supply: '', circulatingSupply: '', burntToken: '', pancakeswap_price: '', volumePerDay: '', liquidity: '', holders: '', marketcapFull: '', ath: '' })
   const MINUTE_MS = 600000;
-  let circulatingSupply;
-  let burntToken;
-  let pancakeswap_price;
-  let volumePerDay;
-  let liquidity;
-  let holders;
-  let marketcapFull;
-  let ath;
-  let total_supply;
-  let max_supply = 2000000000000;
+  const maxSupply = 2000000000000;
 
-  async function getData() {
+  const getAllData = async () => {
+    const provider = new Web3(window.web3.currentProvider);
+    const ccfContract = new provider.eth.Contract(tokenAbi, tokenAddress);
+    const wbnbContract = new provider.eth.Contract(tokenAbi, wbnbTokenAddress);
+    const busdContract = new provider.eth.Contract(tokenAbi, busdTokenAddress);
+    const lpContract = new provider.eth.Contract(ccfWbnbLpTokenAbi, lptokenAddress);
+    const ccfbalanceOfLP = (await ccfContract.methods.balanceOf(lptokenAddress).call()) / 10 ** 9;
+    const wbnbbalanceOfLP = (await wbnbContract.methods.balanceOf(lptokenAddress).call()) / 10 ** 18;
+    const priceOfBNB = (await busdContract.methods.balanceOf("0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16").call()) / (await wbnbContract.methods.balanceOf("0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16").call())
+    const ccfPrice = wbnbbalanceOfLP / ccfbalanceOfLP * priceOfBNB
+    const burnRate = (await ccfContract.methods.brnFee().call()) / 100
+    const totalSupply = (await ccfContract.methods.totalSupply().call()) / 10 ** 9
+    const marketCapFull = ccfPrice * totalSupply
+    const lpTotalSupply = (await lpContract.methods.totalSupply().call()) / 10 ** 18
+    const burntAmount = (await ccfContract.methods.balanceOf(deadAddress).call())  / 10 ** 9;
+    const treasuryAmount = (await ccfContract.methods.balanceOf('0xA7930111Db88dc1bD9cB3197db535B9AefAFdD92').call()) / 10 ** 9
+    const totalBurnt = maxSupply - totalSupply + burntAmount
+    const circulatingSupply = totalSupply - burntAmount + treasuryAmount
+    const marketCap = ccfPrice * circulatingSupply
+    const liquidity = 2 * ccfbalanceOfLP * ccfPrice / lpTotalSupply
+    let volumePerDay = 0
+    const holders = 5818
+    let ath = 0
     await axios.get("https://api.coingecko.com/api/v3/coins/markets", { params },
       { headers: { "Access-Control-Allow-Origin": 'http://localhost:3000' } })
       .then(res => {
-        pancakeswap_price = res.data[0].current_price;
         volumePerDay = res.data[0].total_volume;
-        // liquidity = 152614;
-        holders = 3712;
-        total_supply = res.data[0].total_supply;
-        marketcapFull = pancakeswap_price * total_supply;
         ath = res.data[0].ath;
-        burntToken = max_supply - total_supply + manualburntToken;
-        circulatingSupply = total_supply - burntToken;
-        liquidity = 2 * lpTokenAmount * pancakeswap_price / lpTokenTotalSupply;
-        setToken({ max_supply: max_supply, circulatingSupply: circulatingSupply, burntToken: burntToken, pancakeswap_price: pancakeswap_price, volumePerDay: volumePerDay, liquidity: liquidity, holders: holders, marketcapFull: marketcapFull, ath: ath });
-        setrecentTransactionData([
-          {
-            id: '#5327',
-            name: 'Total supply',
-            date: Number(total_supply).toLocaleString(),
-            avatarColor: '#fa8c16'
-          },
-          {
-            id: '#5328',
-            name: 'Circulating supply',
-            date: Number(circulatingSupply).toLocaleString(),
-            avatarColor: '#04d182'
-          },
-          {
-            id: '#5329',
-            name: 'Burnt token',
-            date: Number(burntToken).toLocaleString(),
-            avatarColor: '#ffc542'
-          }, {
-            id: '#5330',
-            name: 'Burning rate',
-            date: 2 + ' %',
-            avatarColor: '#04d182'
-          },
-          {
-            id: '#5331',
-            name: 'Pancakeswap Price',
-            date: '$ ' + pancakeswap_price,
-            avatarColor: '#04d182'
-          },
-          {
-            id: '#5332',
-            name: '24H Volume',
-            date: '$ ' + Number(volumePerDay).toLocaleString(),
-            avatarColor: '#fa8c16'
-          },
-          {
-            id: '#5333',
-            name: 'Liquidity',
-            date: '$ ' + Number(liquidity).toLocaleString(),
-            avatarColor: '#1890ff'
-          },
-          {
-            id: '#5334',
-            name: 'Holders',
-            date: Number(holders).toLocaleString(),
-            avatarColor: '#ffc542'
-          },
-          {
-            id: '#5335',
-            name: 'Market Cap(Fully Dilluted)',
-            date: '$ ' + Number(marketcapFull).toLocaleString(),
-            avatarColor: '#ff6b72'
-          },
-          {
-            id: '#5336',
-            name: 'All-Time High',
-            date: '$ ' + ath,
-            avatarColor: '#ff6b72'
-          }]);
-        console.log(recentTransactionData);
       })
-      .catch(err => {
-        console.log(err + "first--------------------------");
-      });
-  };
-
-  const getccfTokenData = async () => {
-    const provider = new Web3(window.web3.currentProvider);
-    const ccfContract = new provider.eth.Contract(ccfTokenAbi, tokenAddress);
-    const burntAmount = (await ccfContract.methods.balanceOf(deadAddress).call())  / 10 ** 9;
-    const lpAmount = (await ccfContract.methods.balanceOf(lptokenAddress).call()) / 10 ** 9;
-    setLpTokenAmount(lpAmount);
-    setburntToken(burntAmount);
-  };
-
-  const getLpData = async () => {
-    const provider = new Web3(window.web3.currentProvider);
-    const lpContract = new provider.eth.Contract(ccfWbnbLpTokenAbi, lptokenAddress);
-    const lpTotalSupply = (await lpContract.methods.totalSupply().call()) / Math.pow(10, 18);
-    setLpTokenTotalSupply(lpTotalSupply);
+    setrecentTransactionData([
+      {
+        title: 'Market Cap',
+        value: '$ ' + Number(marketCap).toLocaleString(),
+      },
+      {
+        title: 'CCF/BNB Liquidity',
+        value: '$ ' + Number(liquidity).toLocaleString(),
+      },
+      {
+        title: '24H Volume',
+        value: '$ ' + Number(volumePerDay).toLocaleString(),
+      },
+      {
+        title: 'Total supply',
+        value: Number(totalSupply).toLocaleString(),
+      },
+      {
+        title: 'Circulating supply',
+        value: Number(circulatingSupply).toLocaleString(),
+      },
+      {
+        title: 'Burnt token',
+        value: Number(totalBurnt).toLocaleString(),
+      }, {
+        title: 'Burning rate',
+        value: Number(burnRate).toLocaleString() + ' %',
+      },
+      {
+        title: 'Pancakeswap Price',
+        value: '$ ' + ccfPrice.toFixed(9),
+      },
+      {
+        title: 'Holders',
+        value: Number(holders).toLocaleString(),
+      },
+      {
+        title: 'Market Cap(Fully Dilluted)',
+        value: '$ ' + Number(marketCapFull).toLocaleString(),
+      },
+      {
+        title: 'All-Time High',
+        value: '$ ' + ath,
+      }
+    ]);
   }
   
-  const getPriceData = async () => {
-    await axios.get(`https://api.coingecko.com/api/v3/coins/cross-chain-farming/market_chart?vs_currency=usd&days=1&interval=1s`,
-    { headers: { "Access-Control-Allow-Origin": 'http://localhost:3000' } })
-    .then(res => {
-      const pricesData = res.data.prices.filter((_, index) => index % 10 === 9).map((price, key) => {
-        let openPrice = res.data.prices[key * 10][1];
-        let highPrice = res.data.prices[key * 10][1];
-        let lowPrice = res.data.prices[key * 10][1];
-        let closePrice = res.data.prices[key * 10 + 9][1];
-        for (let i = 0; i <= 9; i++) {
-          if (highPrice < res.data.prices[key * 10 + i][1]) {
-            highPrice = res.data.prices[key * 10 + i][1];
-          }
-          if (lowPrice > res.data.prices[key * 10 + i][1]) {
-            lowPrice = res.data.prices[key * 10 + i][1];
-          }
-        }
-        return {
-          x: new Date(price[0]),
-          y: [openPrice, highPrice, lowPrice, closePrice]
-        }
-      })
-      setSeries([{
-        data: pricesData
-      }])
-    })
-    .catch(err => {
-      console.log(err + "first--------------------------");
-    });
-  }
+  // const getPriceData = async () => {
+  //   await axios.get(`https://api.coingecko.com/api/v3/coins/cross-chain-farming/market_chart?vs_currency=usd&days=1&interval=1s`,
+  //   { headers: { "Access-Control-Allow-Origin": 'http://localhost:3000' } })
+  //   .then(res => {
+  //     const pricesData = res.data.prices.filter((_, index) => index % 10 === 9).map((price, key) => {
+  //       let openPrice = res.data.prices[key * 10][1];
+  //       let highPrice = res.data.prices[key * 10][1];
+  //       let lowPrice = res.data.prices[key * 10][1];
+  //       let closePrice = res.data.prices[key * 10 + 9][1];
+  //       for (let i = 0; i <= 9; i++) {
+  //         if (highPrice < res.data.prices[key * 10 + i][1]) {
+  //           highPrice = res.data.prices[key * 10 + i][1];
+  //         }
+  //         if (lowPrice > res.data.prices[key * 10 + i][1]) {
+  //           lowPrice = res.data.prices[key * 10 + i][1];
+  //         }
+  //       }
+  //       return {
+  //         x: new Date(price[0]),
+  //         y: [openPrice, highPrice, lowPrice, closePrice]
+  //       }
+  //     })
+  //     setSeries([{
+  //       data: pricesData
+  //     }])
+  //   })
+  //   .catch(err => {
+  //     console.log(err + "first--------------------------");
+  //   });
+  // }
 
   useEffect(() => {
-    getccfTokenData();
-    getLpData();
-    getData();
-    getPriceData();
+    getAllData()
     setInterval(() => {
-      getccfTokenData();
-      getLpData();
-      getData();
-      getPriceData();
+      getAllData()
     }, MINUTE_MS);
 
-  }, [manualburntToken]);
+  }, []);
 
   return (
     <>
@@ -263,48 +187,11 @@ export const DefaultDashboard = () => {
         <Col xs={32} sm={32} md={32} lg={24}>
           <Row gutter={40}>
             {
-              annualStatisticData.map((elm, i) => (
-                <Col xs={24} sm={24} md={24} lg={16} xl={8} key={i}>
+              recentTransactionData.map((elm, i) => (
+                <Col xs={24} sm={24} md={24} lg={12} xl={8} key={i}>
                   <StatisticWidget
                     title={elm.title}
-                    value={(i == 0 && token.circulatingSupply != undefined) ? "$ " + (token.circulatingSupply * token.pancakeswap_price).toLocaleString() :
-                      (i == 2 && token.volumePerDay != undefined) ? "$ " + token.volumePerDay.toLocaleString() :
-                        "$ " + token.liquidity.toLocaleString()}
-                    status={elm.status}
-                    subtitle={elm.subtitle}
-                  />
-                </Col>
-              ))
-            }
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <ChartWidget
-                title="$CCF Metrics"
-                extra={'$ ' + token.pancakeswap_price}
-                series={series}
-                height={400}
-                type="candlestick"
-                direction={direction}
-              ></ChartWidget>
-              {/* <Card title="Token Information" extra={cardDropdown(latestTransactionOption)}>
-                <Table
-                  className="no-border-last"
-                  columns={tableColumns}
-                  dataSource={recentTransactionData}
-                  rowKey='id'
-                  pagination={false}
-                />
-              </Card> */}
-            </Col>
-          </Row>
-          <Row gutter={40}>
-            {
-              recentTransactionData.map((elm, i) => (
-                <Col xs={24} sm={24} md={24} lg={16} xl={8} key={i}>
-                  <StatisticWidget
-                    title={elm.name}
-                    value={elm.date}
+                    value={elm.value}
                   />
                 </Col>
               ))
